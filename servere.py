6e07e9e03d28ca6f05d6448c70be021668d51357
Dorian Kita <dorian.kita90@gmail.com>
2025-07-02 19:53:51 +0200

    created url for female and age guessing

--- servere.py
+++ servere.py
@@ -1,10 +1,20 @@
+import datetime
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    year = datetime.datetime.now().year
+    return render_template('index.html', year=year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_data = requests.get(url=f'https://api.genderize.io?name={name}').json()['gender']
+    age_data = requests.get(url = f'https://api.agify.io?name={name}').json()['age']
+
+    return render_template('guess.html', name=name, sex = gender_data, age = age_data)
 
 
 if __name__ == '__main__':
